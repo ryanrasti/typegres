1b3fd2b8d08de9972899e48121978abd30f023eb
Ryan Rasti <ryanrasti@protonmail.com>
2025-08-29 11:08:47 -0700

    stash: types expanding poc

--- src/grammar/alter-table.ts
+++ src/grammar/alter-table.ts
@@ -1,4 +1,5 @@
 type Component = string | readonly Component[] | Builder<any>;
+type Components = readonly Component[];
 
 type Overrides<C extends readonly Component[]> = {};
 
@@ -22,33 +23,54 @@ type ExtractClause<C extends Component> = C extends readonly [infer S, ...any]
 type CamelCase<S extends string> = S extends `${infer P1} ${infer P2}`
   ? `${Lowercase<P1>}${Capitalize<CamelCase<P2>>}`
   : Lowercase<S>;
+type Repeated = readonly [string, "..."] | ["..."];
+type Optional<T = any> = readonly T[];
+type Param = Lowercase<string> | Optional<Lowercase<string>>;
 
-type ExtractParameters<C extends readonly Component[], K extends `${number}` & keyof C> = C[K] extends readonly any[]
-  ? ExtractParametersHelper<C[K], '0'>
-  : ExtractParametersHelper<C, K>;
+// Transitions<L> <- object of { [k in string]: () => Transitions }
+type Transitions<C extends Components, End extends object> = C extends [...any[], infer B, Repeated]
+  ? B extends Builder<any>
+    ? Invoke<B, Transitions<C, End> & End>
+    : [...TransitionsHelper<C, End>, End]
+  : [...TransitionsHelper<C, End>, End];
 
-type ExtractParametersHelper<C extends readonly any[], K extends `${number}`, F = false> = C extends readonly [
-  any,
-  ...infer R,]
-  ? K extends keyof C & `${number}`
-    ? ExtractClause<C[K]> extends never
-      ? [any, ...ExtractParametersHelper<R, K, true>]
-      : F extends true
-        ? []
-        : ExtractParametersHelper<R, K>
-    : []
+type TransitionsHelper<
+  C extends Components,
+  NextTransition extends object,
+  Params extends unknown[] = [],
+> = C extends readonly [...infer F, infer L]
+  ? L extends Param
+    ? WrapTransitionsHelper<F extends Components ? F : never, NextTransition, [unknown, ...Params]>
+    : WrapTransitionsHelper<
+        F extends Components ? F : never,
+        ToCallback<L extends Component ? L : never, L extends Optional ? [] : Params, NextTransition>,
+        L extends Optional ? Params : []
+      >
   : [];
 
-type T = Exclude<Uppercase<string>, "FOO">;
-const t: T = "FOO";
+type WrapTransitionsHelper<
+  C extends Components,
+  NextTransition extends object,
+  Params extends unknown[] = [],
+> = [...TransitionsHelper<C, NextTransition, Params>, NextTransition];
 
-type Callbacks<C extends readonly Component[]> = {
-  [K in keyof C &  `${number}`  as CamelCase<ExtractClause<C[K]>>]:  (...args: ExtractParameters<C, K>) => K;
+type ToCallback<C extends Component, Params extends unknown[], NextTransition extends object> = (C extends string
+  ? { [k in CamelCase<C>]: (...args: Params) => NextTransition }
+  : C extends readonly [string, ...infer T]
+    ? { [k in CamelCase<C[0]>]: (...args: MapUnknown<T>) => NextTransition }
+    : {}) &
+  (C extends Optional ? NextTransition : {});
+
+type MapUnknown<T extends any[]> = {
+  [k in keyof T]: unknown;
 };
 
+type Invoke<B, R> = ["INVOKE", B, R];
+
 const b = ["ALTER TABLE", ["IF EXISTS"], ["ONLY"], "name", ["*"], "FOO", [",", "..."]] as const;
-type R = ExtractParameters<typeof b, 2>;
-type R2 = Callbacks<typeof b>;
+type Expand<T> = T extends object ? { [K in keyof T]: T[K] } : T;
+const t: Expand<Transitions<typeof b, { end: () => {} }>>;
+t[0].alterTable().ifExists().only().
 
 const builder = <C extends Component[], O extends Overrides<C>>(components: C, overrides: O) => {
   return new (class extends Builder<{}> {})({});
@@ -81,3 +103,15 @@ const action = builder([
   ["COLLATE", "collation"],
   ["column_constraint", ["..."]],
 ]);
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+};
+
+const USER_KEYS = ["id", "name", "email"] as const;
+type ObjectToTuple<T, K extends readonly (keyof T)[]> = {
+  [N in keyof K as N extends `${number}` ? N : never]: T[K[N & string]];
+};
+type UserTuple = ObjectToTuple<User, typeof USER_KEYS>;
